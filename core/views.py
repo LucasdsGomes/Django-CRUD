8e8250244af0f939ca8d09cc79a5c8f8d15a4a24
CesarCarriel <cesarcarrieldev@gmail.com>
2023-07-07 19:56:58 -0400

    refactor: removi venv

--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Pessoa
 
+
 def home(request):
     pessoas = Pessoa.objects.all()
     return render(request, 'index.html', {"pessoas": pessoas})
+
 
 def salvar(request):
     vnome = request.POST.get("nome")
@@ -11,16 +13,19 @@ def salvar(request):
     pessoas = Pessoa.objects.all()
     return render(request, 'index.html', {"pessoas": pessoas})
 
+
 def editar(request, id):
     pessoa = Pessoa.objects.get(id=id)
     return render(request, 'update.html', {"pessoa": pessoa})
-    
+
+
 def update(request, id):
     vnome = request.POST.get('nome')
     pessoa = Pessoa.objects.get(id=id)
     pessoa.nome = vnome
     pessoa.save()
     return redirect(home)
+
 
 def delete(request, id):
     pessoa = Pessoa.objects.get(id=id)
